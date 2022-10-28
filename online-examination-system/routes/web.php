264d64a078e65d6ce0669520f22b8ca3aef7e0e3
pyabb <pyabb.89@gmail.com>
2022-10-28 01:02:31 -0500

    OES - rework: routes

--- online-examination-system/routes/web.php
+++ online-examination-system/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ViewsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect('login');
-});
-
-Route::get('/dashboard', function () {
-    return view('templates.dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/', [ViewsController::class, 'home'])->middleware(['auth'])->name('home');
+Route::get('/dashboard', [ViewsController::class, 'dashboard'])->middleware(['auth'])->name('dashboard');
+Route::get('/profile', [ViewsController::class, 'profile'])->middleware(['auth'])->name('profile');
 
 require __DIR__.'/auth.php';
