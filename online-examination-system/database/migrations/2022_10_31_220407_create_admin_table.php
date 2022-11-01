eb0fca570a9b3446ed079d58861ddaaf244c782f
pyabb <pyabb.89@gmail.com>
2022-11-01 16:39:25 -0500

    OES - add: admin login request, admin model, admin table and admin auth session controller

---
+++ online-examination-system/database/migrations/2022_10_31_220407_create_admin_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid', 36)->unique();
+            $table->string('firstname', 255);
+            $table->string('lastname', 255);
+            $table->string('email', 255)->unique();
+            $table->string('documentType', 15);
+            $table->string('document', 15)->unique();
+            $table->string('phone')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->boolean('isAdmin')->default(false);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin');
+    }
+}
