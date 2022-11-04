2815a9cdecf7b4a3bfcca55c9ddf2e9cd3a84384
pyabb <pyabb.89@gmail.com>
2022-11-04 01:43:30 -0500

    OES - add: provisional routes for double login auth

--- online-examination-system/routes/web.php
+++ online-examination-system/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\Admin\AdminAuthenticatedSessionController;
 use App\Http\Controllers\ViewsController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,8 +16,34 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [ViewsController::class, 'home'])->middleware(['auth'])->name('home');
-Route::get('/dashboard', [ViewsController::class, 'dashboard'])->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', [ViewsController::class, 'dashboard'])
+    ->middleware('auth')
+    ->name('dashboard');
 Route::get('/profile', [ViewsController::class, 'profile'])->middleware(['auth'])->name('profile');
 Route::get('/profile/edit', [ViewsController::class, 'profile_edit'])->middleware(['auth'])->name('profile.edit');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+/**
+ * Admin
+ */
+
+Route::get('admin/login', [AdminAuthenticatedSessionController::class, 'create'])
+    ->middleware(['guest:admin', 'guest'])
+    ->name('admin.login');
+
+Route::get('admin/dashboard', [ViewsController::class, 'dashboard'])
+    ->middleware('auth:admin')
+    ->name('admin.dashboard');
+
+Route::get('/admin/profile', [ViewsController::class, 'profile'])
+    ->middleware(['auth:admin'])
+    ->name('admin.profile');
+
+
+Route::post('admin/login', [AdminAuthenticatedSessionController::class, 'store']);
+
+Route::post('admin/logout', [AdminAuthenticatedSessionController::class, 'destroy'])
+    ->middleware('auth:admin')
+    ->name('admin.logout');
+
