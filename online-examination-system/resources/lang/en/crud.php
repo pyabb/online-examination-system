1603ef30b5e2f144dfd9d28223b43f6fdf370a69
pyabb <pyabb.89@gmail.com>
2022-11-06 14:31:47 -0500

    OES - add: crud response messages

---
+++ online-examination-system/resources/lang/en/crud.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'profile' => [
+        'update' => [
+            'success' => '',
+            'error' => 'The profile cannot be updated, try again in few minutes.',
+        ],
+    ],
+];
