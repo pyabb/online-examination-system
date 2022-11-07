825ecacd6a2cbbc579318366a09b7b20619d93e9
pyabb <pyabb.89@gmail.com>
2022-11-06 22:49:34 -0500

    OES - fix: model to update admin and user data

--- online-examination-system/app/Http/Controllers/ProfileController.php
+++ online-examination-system/app/Http/Controllers/ProfileController.php
@@ -7,6 +7,7 @@ use App\Models\Admin;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
 
 class ProfileController extends Controller
@@ -29,13 +30,14 @@ class ProfileController extends Controller
     public function update(ProfilePostRequest $request)
     {
         $validated = $request->validated();
-        $success = Admin::where(['id' => Auth::user()->id])->update($validated);
+        $model = (Auth::user()->isAdmin || Auth::user()->isTeacher) ? '\App\Models\Admin' : '\App\Models\User';
+        $success = $model::where(['id' => Auth::user()->id])->update($validated);
         if(!$success)
         {
             throw ValidationException::withMessages([
                 'profile' => trans('crud.profile.update.error'),
             ]);
         }
-        return redirect()->back()->with('message', 'User is updated successfully.');
+        return redirect()->back()->with('message', 'The ' . Str::of($model)->classBasename() . ' was updated successfully.');
     }
 }
