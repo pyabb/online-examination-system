b75317e9828e4a382c6865f9aa4bcc5ce6a0abaa
pyabb <pyabb.89@gmail.com>
2022-11-07 19:09:43 -0500

    OES - add: student error message in crud file config

--- online-examination-system/resources/lang/en/crud.php
+++ online-examination-system/resources/lang/en/crud.php
@@ -7,4 +7,10 @@ return [
             'error' => 'The profile cannot be updated, try again in few minutes.',
         ],
     ],
+    'student' => [
+        'update' => [
+            'success' => '',
+            'error' => 'The student cannot ne updated, try again in few minutes.'
+        ]
+    ]
 ];
