5858b1c9d021263b4faf3a8c1f7cec5ad31fe867
pyabb <pyabb.89@gmail.com>
2022-11-30 23:06:36 -0500

    OES - add: exam store error message

--- online-examination-system/resources/lang/en/crud.php
+++ online-examination-system/resources/lang/en/crud.php
@@ -16,5 +16,11 @@ return [
             'success' => '',
             'error' => 'The student cannot be updated, try again in few seconds.'
         ]
-    ]
+    ],
+    'exam' => [
+        'store' => [
+            'success' => '',
+            'error' => 'The exam cannot be created, try again in few seconds.',
+        ]
+    ],
 ];
