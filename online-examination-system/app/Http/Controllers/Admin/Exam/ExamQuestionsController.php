7045746ba67cb6bc87769d8c2420c3de73e4cb60
pyabb <pyabb.89@gmail.com>
2022-12-04 16:31:12 -0500

    oes - add: exam questions controller

---
+++ online-examination-system/app/Http/Controllers/Admin/Exam/ExamQuestionsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Exam;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ExamQuestionsController extends Controller
+{
+    public function store()
+    {
+        // todo
+    }
+
+    public function update()
+    {
+        // todo
+    }
+
+    public function delete()
+    {
+        // todo
+    }
+}
