9ed370e8719b764f29ff07cb8ccfac388720de12
pyabb <pyabb.89@gmail.com>
2022-12-05 16:44:26 -0500

    oes - add: edit function in exam questions controller

--- online-examination-system/app/Http/Controllers/Admin/Exam/ExamQuestionsController.php
+++ online-examination-system/app/Http/Controllers/Admin/Exam/ExamQuestionsController.php
@@ -3,11 +3,43 @@
 namespace App\Http\Controllers\Admin\Exam;
 
 use App\Http\Controllers\Controller;
+use App\Models\Exam;
+use App\Models\ExamQuestions;
 use Illuminate\Http\Request;
 
 class ExamQuestionsController extends Controller
 {
-    public function store()
+    public function edit($id, $qid)
+    {
+        $exam = Exam::find($id);
+        if(!$exam)
+        {
+            return abort(404);
+        }
+
+        $question = ExamQuestions::find($qid);
+        if($question->exam->id != $exam->id)
+        {
+            return abort(404);
+        }
+
+        $questions = $exam->allQuestions;
+        $count = 1;
+        foreach ($questions as $q)
+        {
+            var_dump($q->id);
+            if($q->id == $question->id)
+            {
+                $question->relid = $count;
+                break;
+            }
+            $count++;
+        }
+
+        return view('templates.admin.exam-questions', ['exam' => $exam, 'question' => $question, 'questions' => $questions]);
+    }
+
+    public function store(ExamQuestionsRequest $request)
     {
         // todo
     }
