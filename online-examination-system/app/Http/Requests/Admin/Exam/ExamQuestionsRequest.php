7d7f570fa1b6c7e517ed6b4b2afbea32dac75d46
pyabb <pyabb.89@gmail.com>
2022-12-05 16:53:09 -0500

    oes - add: exam questions request

---
+++ online-examination-system/app/Http/Requests/Admin/Exam/ExamQuestionsRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Admin\Exam;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ExamQuestionsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'examId' => ['required', 'digit_between:1,3'],
+            'question' => ['required'],
+        ];
+    }
+}
